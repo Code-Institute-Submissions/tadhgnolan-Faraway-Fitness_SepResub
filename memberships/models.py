e5d115a6683b921d25486b930d084ece43b07ca4
Tadhg N <tadhgnolan@gmail.com>
2022-06-01 21:57:10 +0000

    Add membership views

--- memberships/models.py
+++ memberships/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Membership_type(models.Model):
-    category models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
--- memberships/views.py
+++ memberships/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Membership_type
 
 # Create your views here.
+
+def all_memberships(request):
+        """A view to show3 all product, including sorting and search queries"""
+
+        memberships = Memberships.objects.all()
+
+        context = {
+            'memberships': memberships,
+        }
+
+        return render(request, 'memberships/memberships.html', context)        
