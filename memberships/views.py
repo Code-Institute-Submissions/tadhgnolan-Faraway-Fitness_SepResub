728069ac749fbadffb2db0816151accc6c696e8b
Tadhg N <tadhgnolan@gmail.com>
2022-07-14 13:07:27 +0000

    Add bag app

--- memberships/views.py
+++ memberships/views.py
@@ -5,9 +5,9 @@ from .models import Membership_type
 # Create your views here.
 
 
-def memberships(request):
-    *** A view to return the member page ***
+"""def memberships(request):
+    """ A view to return the member page """
 
     return render(request=request, 
                   template_name='memberships/member.html',
-                  context=['members':Membership_type.objects.all])
+                  context=['members':Membership_type.objects.all]) """
