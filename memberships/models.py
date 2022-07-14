1edbd16495f98efe0b4160069bd61719ad0a2f0c
Tadhg N <tadhgnolan@gmail.com>
2022-07-14 00:08:28 +0000

    Add memberships app

---
+++ memberships/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Membership_type(models.Model):
+    membership_title = models.CharField(max_length=200)
+    membership_description = models.TextField()
+    membership_duration = models.CharField(max_length=2)
+    membership_start = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.membership_title
---
+++ memberships/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Membership_type
+
+# Create your views here.
+
+
+def memberships(request):
+    *** A view to return the member page ***
+
+    return render(request=request, 
+                  template_name='memberships/member.html',
+                  context=['members':Membership_type.objects.all])
