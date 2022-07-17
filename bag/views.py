c516ffdea94699e71e3a9e7be50eb71910476a64
Tadhg N <tadhgnolan@gmail.com>
2022-07-17 16:41:15 +0000

    Add add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,20 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, membership_id):
+    """ Add the specific membership to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if membership_id in list(bag.keys()):
+        bag[membership_id] += quantity
+    else:
+        bag[membership_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
