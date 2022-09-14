3429ac4c96ead12d55ba30d03d002f2f0526a432
Tadhg N <tadhgnolan@gmail.com>
2022-09-14 23:32:52 +0000

    Add stripe public key to checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -20,6 +20,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LN9XuFK3uJBUiyjeJMFsrpzJPOeHNFWOuI8RZ3Vku9uxT2UCaJvnBv8sXjAb6eB8ZVP0iPQstLj0KBOIWVQDxuH00BLxGwJTj',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
