98a5a896e51205101aa384987c71b83b0f70d566
Tadhg N <tadhgnolan@gmail.com>
2022-09-15 20:22:08 +0000

    Add article model to news/models.py

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
@@ -6,3 +7,14 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    headline = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+
+    def __str__(self):
+        return self.headline
