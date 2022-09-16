19ec0f4e420f2827afd08c1a51a40707115f201a
Tadhg N <tadhgnolan@gmail.com>
2022-09-16 20:54:54 +0000

    Add views for delete article and update article to news/views.py

--- news/views.py
+++ news/views.py
@@ -25,7 +25,7 @@ def view_article(request, id):
     return render(request, template, context)
 
 
-# Checks if user is admin / superuser before allowing them to post
+# Checks if user is admin / superuser before allowing them to add article
 @login_required
 def add_article(request):
     if not request.user.is_superuser:
@@ -43,5 +43,39 @@ def add_article(request):
     template = "news/add_article.html"
     context = {
         "form": form,
+    }
+    return render(request, template, context)
+
+# Checks if user is admin / superuser before allowing them to delete article
+@login_required
+def delete_article(request, id):
+    if not request.user.is_superuser:
+        messages.error(request, "Invalid user permission")
+        return redirect(reverse("news"))
+    article = get_object_or_404(Article, id=id)
+    article.delete()
+    messages.success(request, "Article deleted")
+    return redirect(reverse("news"))
+
+# Checks if user is admin / superuser before allowing them to delete article
+@login_required
+def update_article(request, id):
+    if not request.user.is_superuser:
+        messages.error(request, "Invalid user permission")
+        return redirect(reverse("news"))
+    article = get_object_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None, instance=article)
+    if request.method == "POST":
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            messages.success(request, "Article updated")
+            return redirect(reverse("news"))
+        messages.error(request, "Error. Try again.")
+
+    template = 'news/update_article.html'
+    context = {
+        "form": form,
+        "article": article,
     }
     return render(request, template, context)
