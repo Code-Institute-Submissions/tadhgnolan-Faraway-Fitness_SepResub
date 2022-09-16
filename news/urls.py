aab03ea9b18673c354c6193c0c3f35b3699d3067
Tadhg N <tadhgnolan@gmail.com>
2022-09-16 15:08:29 +0000

    Add add_article path to news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.get_articles, name='news'),
-    path('article/<int:id>/', views.view_article, name='view_article')
+    path('article/<int:id>/', views.view_article, name='view_article'),
+    path('articles/add/', views.add_article, name='add_article'),
 ]
