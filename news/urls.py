2a20279211a88d6f07867205bde5da7ebcf4fe67
Tadhg N <tadhgnolan@gmail.com>
2022-09-16 20:56:31 +0000

    Add url patterns for delete_article and update_article to news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.get_articles, name='news'),
     path('article/<int:id>/', views.view_article, name='view_article'),
     path('articles/add/', views.add_article, name='add_article'),
+    path('article/delete/<int:id>/', views.delete_article, name='delete_article'),
+    path('article/update/<int:id>/', views.update_article, name='update_article'),
 ]
