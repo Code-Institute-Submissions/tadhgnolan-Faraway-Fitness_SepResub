f79b932f6a2be5478cd02ee307369fd589f1122d
Tadhg N <tadhgnolan@gmail.com>
2022-09-16 23:55:00 +0000

    Change formatting of static files in settings.py

--- faraway_fitness/settings.py
+++ faraway_fitness/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["localhost", "faraway-fitness.herokuapp.com"]
-X_FRAME_OPTIONS = "SAMEORIGIN"
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -185,9 +184,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
