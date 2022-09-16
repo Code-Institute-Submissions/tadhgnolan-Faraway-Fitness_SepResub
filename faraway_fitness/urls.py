75fab2782d07542c5e3bf4fb8c3a90b29eb7cd20
Tadhg N <tadhgnolan@gmail.com>
2022-09-16 12:59:43 +0000

    Add news page url pattern to faraway_fitness/urls.py

--- faraway_fitness/urls.py
+++ faraway_fitness/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('memberships/', include('memberships.urls')),
     path('checkout/', include('checkout.urls')),
+    path('news/', include('news.urls'), name='news-urls')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
