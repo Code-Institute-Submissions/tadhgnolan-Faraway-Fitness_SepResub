db44d7490be1d8ac29902e99a3314c93672ee90a
Tadhg N <tadhgnolan@gmail.com>
2022-09-17 05:25:13 +0000

    Import Order model into profile view

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display the user's profile. """
