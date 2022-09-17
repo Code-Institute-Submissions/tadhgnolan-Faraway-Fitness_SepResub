7cf5eee1025d197ea70822ebf7c37bbe22c3b446
Tadhg N <tadhgnolan@gmail.com>
2022-09-17 07:13:43 +0000

    Add contact view to home/views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Welcome
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
+from .models import Contact
+from .forms import ContactForm
 # Create your views here.
 
 
@@ -11,3 +16,36 @@ def index(request):
     return render(request=request,
                   template_name='home/index.html',
                   context={'welcomes': Welcome.objects.all})
+
+
+def contact(request):
+    """ A view to return the contact page"""
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        subject = request.POST['subject']
+        message = request.POST['message']
+        email = request.POST['email']
+        sender = "Contact request from" + " " + str(email)	
+        msg_mail = str(message) + ", " + str(sender)
+        if form.is_valid():
+            send_mail(
+                subject,
+                msg_mail,
+                email,
+                [settings.DEFAULT_FROM_EMAIL, ]
+            )
+            form.save()
+            messages.success(request, 'Your message has been delivered.')
+            return redirect(reverse('contact'))
+        else:
+            messages.error(
+                request,
+                'An error has occured while trying to send your message, please make sure the form is filled out corretly.'
+            )
+    else:
+        form = ContactForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'home/contact.html', context)
